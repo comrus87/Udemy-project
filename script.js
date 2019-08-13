978baf302883e2717b1e54e5904fd407b0f1e966
Andrew <nk-andrew1987@mail.ru>
2019-08-13 17:38:27 +0300

    Первое задание

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+let money = +prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let necessarilyPart = prompt('Введите обязательную статью расходов в этом месяце', '');
+let necessarilyCoint = prompt('Во сколько обойдется?', '');
+const MONTH = 30;
+
+let appData = {
+	budget: money,
+	timeData: time,
+    expenses: {
+      necessarilyPart: necessarilyCoint
+    },
+    income: [],
+    savings: false
+};
+
+
+alert(appData.budget/MONTH);
