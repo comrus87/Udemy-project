195b4916e0a0935088a983f311a95d116b406312
Andrew <nk-andrew1987@mail.ru>
2019-08-13 17:59:22 +0300

    Поправлено

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 "use strict";
 
+const MONTH = 30;
 let money = +prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
-let necessarilyPart = prompt('Введите обязательную статью расходов в этом месяце', '');
-let necessarilyCoint = prompt('Во сколько обойдется?', '');
-const MONTH = 30;
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let a2 = prompt('Во сколько обойдется?', '');
+let a3 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let a4 = prompt('Во сколько обойдется?', '');
 
 let appData = {
 	budget: money,
 	timeData: time,
-    expenses: {
-      necessarilyPart: necessarilyCoint
-    },
+    expenses: {},
     income: [],
     savings: false
 };
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
 
 alert(appData.budget/MONTH);
