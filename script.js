9b79601c667ed5f96dc1c2db9751e9045c3c427b
Andrew <nk-andrew1987@mail.ru>
2019-08-14 12:29:20 +0300

    Save3

--- script.js
+++ script.js
@@ -1,85 +1,82 @@
 "use strict";
 
 const MONTH = 30;
-let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+function start () {
+  money = +prompt('Ваш бюджет на месяц?', '');
+  time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+  while (isNaN(money) || money == '' || money == null) {
+  	money = +prompt('Ваш бюджет на месяц?', '');
+  }
+};
+
+start();
 
 let appData = {
 	budget: money,
 	timeData: time,
     expenses: {},
     income: [],
-    savings: false
+    savings: true,
+    optionalExpenses: {}
 };
 
 
-
-for (let i=0; i < 2; i++) {
-  let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-  let b = prompt('Во сколько обойдется?', '');
-  
-  if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && (typeof(a)) != null && 
+function chooseExpenses () {
+  for (let i=0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', '');
+    let b = prompt('Во сколько обойдется?', '');
+    if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && (typeof(a)) != null && 
   	(typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-    console.log('done');
-	appData.expenses[a] = b; 
+      console.log('done');
+	  appData.expenses[a] = b; 
+    } else {
+  	  console.log('error');
+      i--;
+    }
+  };
+};
+
+chooseExpenses();
+
+function detectDayBudget () {
+  appData.moneyPerDay = (appData.budget/MONTH).toFixed();
+  alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+};
+
+detectDayBudget();
+
+function detectLevel () {
+  if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Cредний уровень достатка');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
   } else {
-  	 console.log('error');
-    i--;
+    console.log('Произошла какая-то ошибка');
+  };
+};
+
+detectLevel ();
+
+function checkSavings () {
+  if (appData.savings) {
+	let save = +prompt('Какова сумма накоплений?', '');
+	let percent = +prompt('Под какой процент?', '');
+
+	appData.monthinCome = percent/100/12*percent;
+	alert('Доход в месяц с вашего депозита: ' + appData.monthinCome);
   }
+};
+
+checkSavings();
+
+function chooseOptExpenses () {
+	for (let i = 1; i < 4; i++) {
+  	  let article = prompt('Статья необязательных расходов?', '');
+  	   appData.optionalExpenses[i] = article;
+  	};
 }
-
-
-// // С циклом while
-// let i = 0;
-
-// while (i < 2) {
-//   let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-//   let b = prompt('Во сколько обойдется?', '');
-  
-//   if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && (typeof(a)) != null && 
-//   	(typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//     console.log('done');
-// 	appData.expenses[a] = b; 
-//   } else {
-//   	 console.log('error');
-//     i--;
-//   }
-//   i++;
-// }
-
-
-// с циклом do while
-
-// let i = 0;
-
-// do {
-//   let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-//   let b = prompt('Во сколько обойдется?', '');
-  
-//   if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && (typeof(a)) != null && 
-//   	(typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//     console.log('done');
-// 	appData.expenses[a] = b; 
-//   } else {
-//   	 console.log('error');
-//     i--;
-//   }
-//   i++;
-// } while (i < 2);
-
-
-appData.moneyPerDay = appData.budget/MONTH;
-
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-	 console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	 console.log('Cредний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-	 console.log('Высокий уровень достатка');
-} else {
-	 console.log('Произошла какая-то ошибка');
-}
-
-console.log(appData)
